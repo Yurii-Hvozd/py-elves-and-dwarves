cb2ba6448563fcc1f79f7450c597073fe2cc13c9
Yurii <yura.gvozd.2002@gmail.com>
2026-05-04 15:52:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+from app.players.elves.druid import Druid  # noqa: F401
+from app.players.elves.elf_ranger import ElfRanger  # noqa: F401
+from app.players.dwarves.dwarf_warrior import DwarfWarrior  # noqa: F401
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith  # noqa: F401
+
+
+def calculate_team_total_rating(team: list) -> str:
+    total_rating = 0
+    for player in team:
+        rating = player.get_rating()
+        total_rating += rating
+    return total_rating
+
+
+def elves_concert(elves: list) -> str:
+    for elf in elves:
+        elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list) -> str:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
